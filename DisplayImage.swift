e89027024f578f64df5bb657bfddac047ff7f4df
黃(黄)文定-ㄏㄨㄤˊ ㄨㄣˊㄉㄧㄥˋ <91007091+HuangWending@users.noreply.github.com>
2023-06-06 21:01:42 +0800

    Add files via upload

--- DisplayImage.swift
+++ DisplayImage.swift
@@ -1,52 +1,31 @@
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // 创建一个NSImageView实例并设置其位置和大小
-        let imageView = NSImageView(frame: NSRect(x: 0, y: 0, width: 200, height: 200))
+        // 创建一个UIImageView实例并设置其位置和大小
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
 
-        // 通过指定图片文件路径创建NSImage实例
+        // 通过指定图片文件路径创建UIImage实例
         let imagePath = "您的图片文件路径"
-        let image = NSImage(contentsOfFile: imagePath)
+        if let image = UIImage(contentsOfFile: imagePath) {
+            // 将图片设置为UIImageView的图像
+            imageView.image = image
+        } else {
+            // 图片加载失败时显示默认占位图像
+            imageView.image = UIImage(named: "placeholder")
+        }
 
-        // 将图片设置为NSImageView的图像
-        imageView.image = image
-
-        // 将NSImageView添加到视图控制器的视图层级中
+        // 将UIImageView添加到视图控制器的视图层级中
         self.view.addSubview(imageView)
     }
 }
 
-// 创建一个NSWindowController子类，用作应用程序的主窗口控制器
-class WindowController: NSWindowController {
+// 创建一个UIWindow实例并设置其根视图控制器为ViewController
+let window = UIWindow(frame: UIScreen.main.bounds)
+window.rootViewController = ViewController()
 
-    override func windowDidLoad() {
-        super.windowDidLoad()
-        let viewController = ViewController()
-        self.window?.contentViewController = viewController
-    }
-}
-
-// 创建NSApplicationDelegate子类，用于处理应用程序的生命周期
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-    var windowController: WindowController?
-
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        let window = NSWindow(contentRect: NSRect(x: 0, y: 0, width: 300, height: 300),
-                              styleMask: [.titled, .closable, .miniaturizable, .resizable],
-                              backing: .buffered, defer: false)
-        window.center()
-        windowController = WindowController(window: window)
-        windowController?.window?.makeKeyAndOrderFront(nil)
-    }
-}
-
-// 创建NSApplication实例并设置其代理
-let application = NSApplication.shared
-let delegate = AppDelegate()
-application.delegate = delegate
-application.run()
+// 设置主窗口并显示
+window.makeKeyAndVisible()
